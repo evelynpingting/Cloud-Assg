a9845a916f69a83c31a7d2f17a40f587b870ff7f
evelynpingting <liongpt-wp20@student.tarc.edu.my>
2023-05-06 00:06:58 +0800

    test

--- EmpApp.py
+++ EmpApp.py
@@ -90,7 +90,7 @@ def AddEmp():
     print("all modification done...")
     return render_template('AddEmpOutput.html', name=emp_name)
 
-@app.route("/ApplyLeaveEmp", methods=['GET', 'POST'])
+@app.route("/ApplyLeaveEmp", methods=['POST'])
 def ApplyLeaveEmp():
     emp_id = request.form['emp_id']
     type_leave = request.form['type_leave']
